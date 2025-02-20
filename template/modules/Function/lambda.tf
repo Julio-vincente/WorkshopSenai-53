b51692a54e97210f453a91f979cdddd1ec7e08d0
Julio Vicente <juliomacedo.vicente@gmail.com>
2025-02-20 10:41:33 -0300

    teste workflow

--- template/modules/Function/lambda.tf
+++ template/modules/Function/lambda.tf
@@ -5,7 +5,7 @@ resource "aws_lambda_function" "kinesis_processor" {
   handler       = "lambda_function.lambda_handler"
   timeout       = 60
   
-  filename         = "lambda_function.py"
+  filename         = "lambda_function.zip"
   # source_code_hash = filebase64sha256("C:/Users/julio/Documents/Olimpiadas/WorkshopSenai-53/template/modules/Function/lambda_function.zip")
   
   environment {
